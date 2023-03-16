4d47121ef7c359f0ff04b49b673cc11223571d29
Wedson Almeida Filho <walmeida@microsoft.com>
2023-03-16 18:18:28 -0300

    osbuilder: install device-mapper and enable udevd
    This is needed when enabling dm-verity. `udevd` reads kernel uevents
    that announce the creation of `/dev/dm-XXX` devices, and then creates
    devices with the actual names under `/dev/mapper/`.
    
    Signed-off-by: Wedson Almeida Filho <walmeida@microsoft.com>

--- tools/osbuilder/rootfs-builder/cbl-mariner/config.sh
+++ tools/osbuilder/rootfs-builder/cbl-mariner/config.sh
@@ -7,6 +7,6 @@
 OS_NAME=cbl-mariner
 OS_VERSION=${OS_VERSION:-3.0}
 LIBC="gnu"
-PACKAGES="kata-packages-uvm"
+PACKAGES="core-packages-base-image ca-certificates device-mapper"
 [ "$AGENT_INIT" = no ] && PACKAGES+=" systemd"
 [ "$SECCOMP" = yes ] && PACKAGES+=" libseccomp"
