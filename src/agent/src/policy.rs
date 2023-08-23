6803b601a4a5dedc0aa7add09ddda6f887ef851e
Dan Mihai <Daniel.Mihai@microsoft.com>
2023-08-23 11:08:28 -0700

    runtime: agent: SNP HOST_DATA set-up (#43)

--- src/agent/src/policy.rs
+++ src/agent/src/policy.rs
@@ -4,7 +4,10 @@
 //
 
 use anyhow::Result;
+use hex;
 use protobuf::MessageDyn;
+use sha2::{Digest, Sha256};
+use slog::Drain;
 use tokio::io::AsyncWriteExt;
 
 use crate::rpc::ttrpc_error;
@@ -145,6 +148,8 @@ impl AgentPolicy {
 
     /// Replace the Policy in regorus.
     pub async fn set_policy(&mut self, policy: &str) -> Result<()> {
+        check_policy_hash(policy)?;
+
         self.engine = Self::new_engine();
         self.engine
             .add_policy("agent_policy".to_string(), policy.to_string())?;
@@ -191,4 +196,14 @@ impl AgentPolicy {
         };
         Ok(())
     }
+}
+
+pub fn check_policy_hash(policy: &str) -> Result<()> {
+    let mut hasher = Sha256::new();
+    hasher.update(policy.as_bytes());
+    let digest = hasher.finalize();
+    debug!(sl!(), "New policy hash: {}", hex::encode(digest));
+
+    // TODO: check that the corresponding TEE field matches this hash.
+    Ok(())
 }
