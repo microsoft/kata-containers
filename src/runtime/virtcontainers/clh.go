12012681fdee35107c11c5e97155651fe5150378
Aurélien Bombo <abombo@microsoft.com>
2023-12-18 19:38:33 +0000

    Revert "runtime: use 10 PCI segments for the Guest (#59)"
    This reverts 544bbe2 as it was already done in a more flexible way
    earlier in 8ea33f3.

--- src/runtime/virtcontainers/clh.go
+++ src/runtime/virtcontainers/clh.go
@@ -468,8 +468,6 @@ func (clh *cloudHypervisor) enableProtection() error {
 			clh.vmconfig.Payload.SetHostData(snpZeroHostData)
 		}
 
-		clh.vmconfig.Platform.SetNumPciSegments(10)
-
 		return nil
 
 	default:
