3ae38f89d2d2614e1f05def7b1ae55e71e473cff
Mitch Zhu <zhumc11@gmail.com>
2023-12-18 14:17:17 -0800

    Add kata metapackage change

--- tools/osbuilder/rootfs-builder/cbl-mariner/config.sh
+++ tools/osbuilder/rootfs-builder/cbl-mariner/config.sh
@@ -5,6 +5,6 @@
 OS_NAME=cbl-mariner
 OS_VERSION=${OS_VERSION:-3.0}
 LIBC="gnu"
-PACKAGES="kata-packages-uvm"
+PACKAGES="kata-packages-coco-uvm"
 [ "$AGENT_INIT" = no ] && PACKAGES+=" systemd"
 [ "$SECCOMP" = yes ] && PACKAGES+=" libseccomp"
