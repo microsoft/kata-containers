27f3aad866c7ece3afdf2a308dfdd62956194e3a
Dan Mihai <dmihai@microsoft.com>
2024-01-13 01:53:42 +0000

    genpolicy: "cargo fmt -- --check" clean-up
    Fixes: #8816
    
    Signed-off-by: Dan Mihai <dmihai@microsoft.com>

--- src/tools/genpolicy/src/pod.rs
+++ src/tools/genpolicy/src/pod.rs
@@ -6,6 +6,7 @@
 // Allow K8s YAML field names.
 #![allow(non_snake_case)]
 
+use crate::agent;
 use crate::config_map;
 use crate::obj_meta;
 use crate::policy;
@@ -15,7 +16,6 @@ use crate::settings;
 use crate::utils::Config;
 use crate::volume;
 use crate::yaml;
-use crate::agent;
 
 use async_trait::async_trait;
 use log::{debug, warn};
