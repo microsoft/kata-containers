e0ace5a47dbd6371ad835639e463692c4414bc31
Dan Mihai <dmihai@microsoft.com>
2024-12-13 23:10:58 +0000

    disable MinHypervisorMemory

--- src/runtime/virtcontainers/hypervisor.go
+++ src/runtime/virtcontainers/hypervisor.go
@@ -82,7 +82,8 @@ const (
 	vSockLogsPort = 1025
 
 	// MinHypervisorMemory is the minimum memory required for a VM.
-	MinHypervisorMemory = 64
+	// MinHypervisorMemory = 64
+	MinHypervisorMemory = 0
 
 	defaultMsize9p = 8192
 
