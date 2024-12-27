7fdcc671c40815edb0eafbf4f9e9724bcc4091fd
Dan Mihai <dmihai@microsoft.com>
2024-12-27 23:04:10 +0000

    runtime: virtio-blk root device for AKS-Kata
    Use virtio-blk for the AKS-Kata Pod VM root storage device. When using
    "--pmem discard_writes=on" the entire Pod VM image gets copied into the
    Pod VM memory, thus resulting in higher memory consumption.
    
    AKS-CC was already using virtio-blk (not pmem).
    
    Signed-off-by: Dan Mihai <dmihai@microsoft.com>

--- src/runtime/pkg/katautils/config.go
+++ src/runtime/pkg/katautils/config.go
@@ -1088,6 +1088,7 @@ func newClhHypervisorConfig(h hypervisor) (vc.HypervisorConfig, error) {
 		FileBackedMemRootList:          h.FileBackedMemRootList,
 		Debug:                          h.Debug,
 		DisableNestingChecks:           h.DisableNestingChecks,
+		DisableImageNvdimm:             h.DisableImageNvdimm,
 		BlockDeviceDriver:              blockDriver,
 		BlockDeviceCacheSet:            h.BlockDeviceCacheSet,
 		BlockDeviceCacheDirect:         h.BlockDeviceCacheDirect,
