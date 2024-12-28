1273ccf95c38ad5270fc4caf3644856d584d977d
Dan Mihai <dmihai@microsoft.com>
2024-12-28 00:20:10 +0000

    DEFMEMSZ=0 DEFSTATICSANDBOXWORKLOADMEM=512

--- src/runtime/pkg/katautils/config.go
+++ src/runtime/pkg/katautils/config.go
@@ -1964,9 +1964,11 @@ func checkHypervisorConfig(config vc.HypervisorConfig) error {
 
 	memSizeMB := int64(config.MemorySize)
 
+	/*
 	if memSizeMB == 0 {
 		return errors.New("VM memory cannot be zero")
 	}
+	*/
 
 	mb := int64(1024 * 1024)
 
