7cd0eec89d8d440dd6427a1af093667ebe85ab1e
Dan Mihai <dmihai@microsoft.com>
2025-01-03 19:45:56 +0000

    image: delete modprobe@ services
    Unsupported on Guest images.
    
    Signed-off-by: Dan Mihai <dmihai@microsoft.com>

--- tools/osbuilder/image-builder/image_builder.sh
+++ tools/osbuilder/image-builder/image_builder.sh
@@ -54,6 +54,7 @@ readonly dax_alignment=2
 # The list of systemd units and files that are not needed in Kata Containers
 readonly -a systemd_units=(
 	"blk-availability"
+	"modprobe@"
 	"systemd-coredump@"
 	"systemd-journald"
 	"systemd-journald-dev-log"
