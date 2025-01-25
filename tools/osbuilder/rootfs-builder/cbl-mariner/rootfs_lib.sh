31be273dd98d273f3e04929e28d812793a79a095
Dan Mihai <dmihai@microsoft.com>
2025-01-25 21:24:14 +0000

    rootfs: remove xkeyboard-config

--- tools/osbuilder/rootfs-builder/cbl-mariner/rootfs_lib.sh
+++ tools/osbuilder/rootfs-builder/cbl-mariner/rootfs_lib.sh
@@ -22,5 +22,8 @@ build_rootfs()
 	info "install packages for rootfs"
 	$DNF install ${EXTRA_PKGS} ${PACKAGES}
 
+	info "Removing packages for rootfs"
+	$DNF remove xkeyboard-config
+	
 	rm -rf ${ROOTFS_DIR}/usr/share/{bash-completion,cracklib,doc,info,locale,man,misc,pixmaps,terminfo,zoneinfo,zsh}
 }
