e45df3172ade29449fa70dc3e2c5f7c94c813f81
Dan Mihai <dmihai@microsoft.com>
2025-01-25 19:10:29 +0000

    rootfs: delete /usr/sbin/lvm
    Files size: 2789376
    
    Signed-off-by: Dan Mihai <dmihai@microsoft.com>

--- tools/osbuilder/rootfs-builder/rootfs.sh
+++ tools/osbuilder/rootfs-builder/rootfs.sh
@@ -98,6 +98,7 @@ readonly -a systemd_units=(
 readonly -a systemd_files=(
 	"blkdeactivate"
 	"journalctl"
+	"lvm"
 	"sys-fs-fuse-connections.mount"
 	"sys-kernel-config.mount"
 	"systemd-bless-boot-generator"
