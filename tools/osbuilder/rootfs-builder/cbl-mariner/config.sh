1109700a60fd1d7ae85a95fe6903cddd66b1c667
Dan Mihai <dmihai@microsoft.com>
2025-01-25 19:44:07 +0000

    rootfs: start reviewing the packages

--- tools/osbuilder/rootfs-builder/cbl-mariner/config.sh
+++ tools/osbuilder/rootfs-builder/cbl-mariner/config.sh
@@ -5,7 +5,35 @@
 OS_NAME=cbl-mariner
 OS_VERSION=${OS_VERSION:-3.0}
 LIBC="gnu"
-PACKAGES="kata-packages-uvm findutils"
+
+#PACKAGES="kata-packages-uvm findutils"
+PACKAGES="\
+    bash \
+    ca-certificates \
+    chrony \
+    cpio \
+    cryptsetup \
+    curl \
+    dbus \
+    elfutils-libelf \
+    filesystem \
+    grep \
+    gzip \
+    iptables \
+    iproute \
+    iputils \
+    irqbalance \
+    lvm2 \
+    lz4 \
+    procps-ng \
+    readline \
+    sed \
+    systemd \
+    tar \
+    tzdata \
+    util-linux \
+    zlib"
+
 [ "$CONF_GUEST" = yes ] && PACKAGES+=" kata-packages-uvm-coco"
 [ "$AGENT_INIT" = no ] && PACKAGES+=" systemd"
 [ "$SECCOMP" = yes ] && PACKAGES+=" libseccomp"
