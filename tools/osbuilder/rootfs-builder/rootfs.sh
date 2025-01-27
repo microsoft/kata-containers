48a5163366b2f5a694c8e20e3c3221c2f38b8b5d
Dan Mihai <dmihai@microsoft.com>
2025-01-27 19:49:41 +0000

    rootfs: delete the remaining systemd-tmpfiles files
    Delete:
    - systemd-tmpfiles-clean.timer
    - systemd-tmpfiles-clean.service
    - systemd-tmpfiles-setup-dev.service
    - systemd-tmpfiles-setup-dev-early.service
    - systemd-tmpfiles
    
    systemd-tmpfiles-setup.service was already deleted.
    
    Signed-off-by: Dan Mihai <dmihai@microsoft.com>

--- tools/osbuilder/rootfs-builder/rootfs.sh
+++ tools/osbuilder/rootfs-builder/rootfs.sh
@@ -54,7 +54,10 @@ readonly -a systemd_units=(
 	"systemd-journal-flush"
 	"systemd-random-seed"
 	"systemd-timesyncd"
+	"systemd-tmpfiles-clean"
 	"systemd-tmpfiles-setup"
+	"systemd-tmpfiles-setup-dev"
+	"systemd-tmpfiles-setup-dev-early"
 	"systemd-update-utmp"
 )
 
@@ -63,6 +66,7 @@ readonly -a systemd_files=(
 	"systemd-fstab-generator"
 	"systemd-getty-generator"
 	"systemd-gpt-auto-generator"
+	"systemd-tmpfiles"
 )
 
 handle_error() {
