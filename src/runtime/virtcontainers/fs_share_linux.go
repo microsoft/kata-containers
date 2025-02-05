7129ea5fabca2ac3f1434c2815e6560771460166
Dan Mihai <dmihai@microsoft.com>
2025-02-05 20:21:29 +0000

    Use host's destination path temporarily

--- src/runtime/virtcontainers/fs_share_linux.go
+++ src/runtime/virtcontainers/fs_share_linux.go
@@ -968,7 +968,7 @@ func (f *FilesystemShare) copyMountSourceRegularFile(ctx context.Context, c *Con
 		"randomBytes": randomBytes,
 	}).Debug("copyMountSourceRegularFile: sending request")
 
-	dstPath, err := f.sandbox.agent.copyFile(
+	_, err := f.sandbox.agent.copyFile(
 		ctx, 
 		srcPath, 
 		requestType,
@@ -983,7 +983,6 @@ func (f *FilesystemShare) copyMountSourceRegularFile(ctx context.Context, c *Con
 		return err
 	}
 
-	/*
     dstPath :=
         "/run/kata-containers/shared/containers/" +
         containerId +
@@ -991,7 +990,6 @@ func (f *FilesystemShare) copyMountSourceRegularFile(ctx context.Context, c *Con
         randomBytes +
         "-" +
         mountDest;
-	*/
 
 	f.Logger().WithField("srcPath", srcPath).WithField("dstPath", dstPath).Info("copyMountSourceRegularFile: Adding (srcPath, dstPath) to srcDstMap")
 	f.srcDstMap[srcPath] = append(f.srcDstMap[srcPath], dstPath)
@@ -1108,7 +1106,7 @@ func (f *FilesystemShare) copyMountSourceDir(ctx context.Context, c *Container, 
 		"randomBytes": randomBytesStr,
 	}).Debug("copyMountSourceDir: sending request")
 
-	dstPath, err := f.sandbox.agent.copyFile(
+	_, err = f.sandbox.agent.copyFile(
 		ctx, 
 		timestampedPath,
 		requestType,
@@ -1123,15 +1121,13 @@ func (f *FilesystemShare) copyMountSourceDir(ctx context.Context, c *Container, 
 		return err
 	}
 
-	/*
 	dstPath :=
 		"/run/kata-containers/shared/containers/" +
 		containerId +
 		"-" +
 		randomBytesStr +
 		"-" +
-		mountDest;
-	*/
+		mountDest
 
 	f.Logger().WithField("srcPath", srcPath).WithField("dstPath", dstPath).Info("copyMountSourceDir: Adding (srcPath, dstPath) to srcDstMap")
 	f.srcDstMap[srcPath] = append(f.srcDstMap[srcPath], dstPath)
