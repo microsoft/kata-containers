3639233a4f8c6658692a6908f9892d1dccfe4969
Dan Mihai <dmihai@microsoft.com>
2025-02-07 01:34:42 +0000

    clean-up

--- src/agent/src/mount.rs
+++ src/agent/src/mount.rs
@@ -1108,6 +1108,7 @@ impl MountState {
         }
     }
 
+    /*
     pub fn get_mapping(&self, container_id: &str, host_path: &str) -> Option<PathBuf> {
         if let Some(c_state) = self.containers_state.get(container_id) {
             c_state.get_mapping(host_path)
@@ -1115,4 +1116,5 @@ impl MountState {
             None
         }
     }
+    */
 }
