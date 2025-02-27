e59593f0fc39a186fd625816ae14bd1e2f953500
Saul Paredes <saulparedes@microsoft.com>
2025-02-27 09:25:40 -0800

    tests: fix regex used in settings for testing genpolicy
    JOB_COMPLETION_INDEX might be empty, so the regex should be changed to match the empty string.
    
    Signed-off-by: Saul Paredes <saulparedes@microsoft.com>

--- src/tools/genpolicy/tests/adapt_settings_for_tests.sh
+++ src/tools/genpolicy/tests/adapt_settings_for_tests.sh
@@ -7,7 +7,7 @@
 # usage: ./tests/adapt_settings_for_tests.sh
 
 jq '.request_defaults.CreateContainerRequest.allow_env_regex_map = {  
-  "JOB_COMPLETION_INDEX": "^[0-9]+$",
+  "JOB_COMPLETION_INDEX": "^[0-9]*$",
   "CPU_LIMIT": "^[0-9]+$",
   "MEMORY_LIMIT": "^[0-9]+$"
 }' genpolicy-settings.json > tmp-genpolicy-settings.json && mv tmp-genpolicy-settings.json genpolicy-settings.json
