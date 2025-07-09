f4f1002663f3d9480c2951c5703f6b1b47dfbbf0
Dan Mihai <dmihai@microsoft.com>
2025-07-09 20:47:31 +0000

    disable guest_pull for non_coco

--- tests/integration/kubernetes/tests_common.sh
+++ tests/integration/kubernetes/tests_common.sh
@@ -99,7 +99,8 @@ adapt_common_policy_settings_for_non_coco() {
 	local settings_dir=$1
 
 	info "Adapting common policy settings for non-CoCo guest"
-	jq '.request_defaults.UpdateEphemeralMountsRequest = true' "${settings_dir}/genpolicy-settings.json" > temp.json
+	jq '.request_defaults.UpdateEphemeralMountsRequest = true | .cluster_config.guest_pull = false' \
+		"${settings_dir}/genpolicy-settings.json" > temp.json
 	sudo mv temp.json "${settings_dir}/genpolicy-settings.json"
 }
 
