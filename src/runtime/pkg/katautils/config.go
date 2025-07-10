9a045a918e03a83c7b0765b7fe0ccec00ac285eb
Cameron Baird <cameronbaird@microsoft.com>
2025-07-10 21:13:18 +0000

    runtime: Relax constraints on factory config
    The constraints are overly harsh because all hypervisor implementations implement Create/Start/PauseVM needed for basic VM caching. The constraint should be on templating which requires additional extra-interface functions.
    
    Signed-off-by: Cameron Baird <cameronbaird@microsoft.com>

--- src/runtime/pkg/katautils/config.go
+++ src/runtime/pkg/katautils/config.go
@@ -1908,14 +1908,11 @@ func checkNetNsConfig(config oci.RuntimeConfig) error {
 // checkFactoryConfig ensures the VM factory configuration is valid.
 func checkFactoryConfig(config oci.RuntimeConfig) error {
 	if config.FactoryConfig.Template {
-		if config.HypervisorConfig.InitrdPath == "" {
-			return errors.New("Factory option enable_template requires an initrd image")
-		}
-	}
-
-	if config.FactoryConfig.VMCacheNumber > 0 {
 		if config.HypervisorType != vc.QemuHypervisor {
 			return errors.New("VM cache just support qemu")
+		}
+		if config.HypervisorConfig.InitrdPath == "" {
+			return errors.New("Factory option enable_template requires an initrd image")
 		}
 	}
 
