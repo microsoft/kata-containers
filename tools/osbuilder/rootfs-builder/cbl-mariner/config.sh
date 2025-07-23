97b2ca6091e59b0a2adf9be13c8eb1454fb7c70b
Cameron Baird <cameronbaird@microsoft.com>
2025-07-23 23:35:01 +0000

    More packages

--- tools/osbuilder/rootfs-builder/cbl-mariner/config.sh
+++ tools/osbuilder/rootfs-builder/cbl-mariner/config.sh
@@ -7,6 +7,6 @@
 OS_NAME=cbl-mariner
 OS_VERSION=${OS_VERSION:-3.0}
 LIBC="gnu"
-PACKAGES="bash util-linux shadow-utils ca-certificates chrony cryptsetup dbus elfutils-libelf filesystem iptables irqbalance systemd tzdata zlib sudo tdnf binutils"
+PACKAGES="bash util-linux shadow-utils ca-certificates chrony cryptsetup dbus elfutils-libelf filesystem iptables irqbalance systemd tzdata zlib sudo tdnf binutils pciutils procps-ng net-tools iproute strace lsof socat nc"
 [ "$AGENT_INIT" = no ] && PACKAGES+=" systemd"
 [ "$SECCOMP" = yes ] && PACKAGES+=" libseccomp"
