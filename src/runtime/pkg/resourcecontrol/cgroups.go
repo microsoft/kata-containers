afa715d28ec8fac6f180ce04d337024d4b7b46e2
Manuel Huber <mahuber@microsoft.com>
2025-07-25 09:46:13 -0700

    Revert "runtime: fix error when using the debug console"
    This reverts commit 3cfdd53a88ee4f9704c957895b633601c4ac4475.

--- src/runtime/pkg/resourcecontrol/cgroups.go
+++ src/runtime/pkg/resourcecontrol/cgroups.go
@@ -61,7 +61,6 @@ func sandboxDevices() []specs.LinuxDeviceCgroup {
 		"/dev/zero",
 		"/dev/urandom",
 		"/dev/console",
-		"/dev/ptmx",
 	}
 
 	// Processes running in a device-cgroup are constrained, they have acccess
