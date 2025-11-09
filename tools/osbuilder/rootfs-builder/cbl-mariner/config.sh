271e1bfd8049639c42d6a4aaa81a89d7fcca698b
Dan Mihai <dmihai@microsoft.com>
2025-11-09 15:40:00 +0000

    add build packages

--- tools/osbuilder/rootfs-builder/cbl-mariner/config.sh
+++ tools/osbuilder/rootfs-builder/cbl-mariner/config.sh
@@ -10,3 +10,18 @@ LIBC="gnu"
 PACKAGES="kata-packages-uvm"
 [ "$AGENT_INIT" = no ] && PACKAGES+=" systemd"
 [ "$SECCOMP" = yes ] && PACKAGES+=" libseccomp"
+
+PACKAGES+=" ca-certificates"
+PACKAGES+=" cronie-anacron"
+PACKAGES+=" logrotate"
+PACKAGES+=" core-packages-base-image"
+
+PACKAGES+=" pciutils"
+PACKAGES+=" awk"
+PACKAGES+=" tar"
+PACKAGES+=" gzip"
+PACKAGES+=" zstd"
+
+PACKAGES+=" build-essential"
+PACKAGES+=" elfutils-libelf-devel"
+PACKAGES+=" kernel-devel-6.6.104.2"
