d58b3ff0899ec8cca0fb21d3b07a8f6d4055d5fb
Dan Mihai <dmihai@microsoft.com>
2025-11-12 03:19:14 +0000

    build and load driver

--- tools/osbuilder/rootfs-builder/cbl-mariner/azl_chroot.sh
+++ tools/osbuilder/rootfs-builder/cbl-mariner/azl_chroot.sh
@@ -23,45 +23,32 @@ build_nvidia_drivers() {
 	driver_source_files="NVIDIA-Linux-x86_64-580.95.05/kernel-open"
 
 	echo "chroot: Build NVIDIA drivers"
-	pushd "${driver_source_files}" >> /dev/null
+	pushd "${driver_source_files}"
 
-	local certs_dir
-	local kernel_version
-	local ARCH
-	for version in /lib/modules/*; do
-		kernel_version=$(basename "${version}")
-		certs_dir=/lib/modules/"${kernel_version}"/build/certs
-		signing_key=${certs_dir}/signing_key.pem
+	make -j "$(nproc)" CC=gcc SYSSRC=/CBL-Mariner-Linux-Kernel && \
+	make INSTALL_MOD_STRIP=1 -j "$(nproc)" CC=gcc SYSSRC=/CBL-Mariner-Linux-Kernel modules_install
 
-	    echo "chroot: Building GPU modules for: ${kernel_version}"
-		cp /boot/System.map-"${kernel_version}" /lib/modules/"${kernel_version}"/build/System.map
-
-		ln -sf /lib/modules/"${kernel_version}"/build/arch/x86 /lib/modules/"${kernel_version}"/build/arch/amd64
-		ARCH=x86_64
-
-		echo "chroot: Building GPU modules for: ${kernel_version} ${ARCH}"
-
-		make -j "$(nproc)" CC=gcc SYSSRC=/lib/modules/"${kernel_version}"/build > /dev/null
-
-		if [[ -n "${KBUILD_SIGN_PIN}" ]]; then
-			mkdir -p "${certs_dir}" && mv /signing_key.* "${certs_dir}"/.
-			check_kernel_sig_config
-		fi
-
-		make INSTALL_MOD_STRIP=1 -j "$(nproc)" CC=gcc SYSSRC=/lib/modules/"${kernel_version}"/build modules_install
-		make -j "$(nproc)" CC=gcc SYSSRC=/lib/modules/"${kernel_version}"/build clean > /dev/null
-		# The make clean above should clear also the certs directory but just in case something
-		# went wrong make sure the signing_key.pem is removed
-		[[ -e "${signing_key}" ]] && rm -f "${signing_key}"
-	done
-	popd >> /dev/null
+	popd
 }
 
+azl_clean_up() {
+	tdnf erase -y \
+elfutils-libelf-devel \
+openssl-devel \
+build-essential \
+git \
+glib-devel \
+openssl-devel
+
+	tdnf clean -y all
+
+	rm /driver.run
+	rm -r /NVIDIA-*
+	rm -rf /CBL-Mariner*
+}
 
 driver_type="-open"
 run_file_name="driver.run"
 prepare_run_file_drivers
 build_nvidia_drivers
-
-#wget https://developer.download.nvidia.cn/compute/cuda/redist/fabricmanager/linux-x86_64/fabricmanager-linux-x86_64-580.95.05-archive.tar.xz
-
+azl_clean_up
