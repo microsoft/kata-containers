2bb4f31d3f0bc4d1a779edecad8da2f40e40cddd
Saul Paredes <saulparedes@microsoft.com>
2026-03-30 08:52:47 -0700

    runtime-rs: improved memory overhead management
    This is a port from https://github.com/microsoft/kata-containers/commit/7ddec33642c9a307d4af79078c2f15be887bec86 into runtime-rs
    
    After these changes:
    
    1. The value of the K8s runtime class memory overhead:
       - Covers the memory usage from all the Host-side components (mainly
         the Kata Shim and the VMM).
       - Doesn't include the memory usage from any Guest-side components.
    
    2. The value of a pod memory limit specified by the user:
       - Is equal to the memory size of the Pod VM.
       - Includes the memory usage from all the Guest-side components
         (mainly user's workload, the Guest kernel, and the Kata Agent)
       - Doesn't include the memory usage from any Host-side components.
    
    Signed-off-by: Saul Paredes <saulparedes@microsoft.com>

--- tools/osbuilder/node-builder/azure-linux/package_build.sh
+++ tools/osbuilder/node-builder/azure-linux/package_build.sh
@@ -40,7 +40,9 @@ runtime_rs_make_flags="BUILD_TYPE=release \
 	QEMUCMD= \
 	FCCMD= \
 	DEFVIRTIOFSDAEMON=${VIRTIOFSD_BINARY_LOCATION} \
-	PREFIX=${INSTALL_PATH_PREFIX}"
+	PREFIX=${INSTALL_PATH_PREFIX}" \
+	DEFMEMSZ=0 \
+	DEFSTATICSANDBOXWORKLOADMEM=512
 
 # - for vanilla Kata we use the kernel binary. For ConfPods we use IGVM, so no need to provide kernel path.
 # - for vanilla Kata we explicitly set DEFSTATICRESOURCEMGMT_CLH. For ConfPods,
