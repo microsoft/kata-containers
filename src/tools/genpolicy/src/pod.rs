04f03003505500fc10088e3dd24541eafded80d2
Dan Mihai <dmihai@microsoft.com>
2026-05-05 15:50:13 +0000

    genpolicy: ignore pod subdomain field

--- src/tools/genpolicy/src/pod.rs
+++ src/tools/genpolicy/src/pod.rs
@@ -107,6 +107,9 @@ pub struct PodSpec {
 
     #[serde(skip_serializing_if = "Option::is_none")]
     schedulerName: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    subdomain: Option<String>,
 }
 
 /// See Reference / Kubernetes API / Workload Resources / Pod.
