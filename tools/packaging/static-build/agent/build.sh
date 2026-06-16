10741568cc463c5f7e99633e4bae48949f8634a4
Alex Lyn <alex.lyn@antgroup.com>
2026-06-16 15:30:30 +0800

    packaging: Add libdevmapper-dev and GNU target to agent Dockerfile
    Install libdevmapper-dev and pkg-config in the agent build container
    so devicemapper-sys can link against libdevmapper. Add the GNU libc
    rustup target alongside musl since USE_DEVMAPPER forces LIBC=gnu.
    
    Forward USE_DEVMAPPER through build.sh and build-static-agent.sh.
    And you can compile the device mapper in kata-agent as below:
    
    ```
    $ make LIBC=gnu USE_DEVMAPPER=yes
    ```
    
    Signed-off-by: Alex Lyn <alex.lyn@antgroup.com>
    (cherry picked from commit 562c9acdb20af570157efbadb028b3a81932e5a4)

--- tools/packaging/static-build/agent/build.sh
+++ tools/packaging/static-build/agent/build.sh
@@ -32,6 +32,7 @@ docker run --rm -i -v "${repo_root_dir}:${repo_root_dir}" \
 	--env AGENT_POLICY="${AGENT_POLICY:-no}" \
 	--env STRICT_POLICY="${STRICT_POLICY:-no}" \
 	--env INIT_DATA="${INIT_DATA:-yes}" \
+	--env USE_DEVMAPPER="${USE_DEVMAPPER:-no}" \
 	--env LIBSECCOMP_VERSION="${LIBSECCOMP_VERSION}" \
 	--env LIBSECCOMP_URL="${LIBSECCOMP_URL}" \
 	--env GPERF_VERSION="${GPERF_VERSION}" \
