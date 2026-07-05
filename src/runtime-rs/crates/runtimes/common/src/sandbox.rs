88cf5477eb4c3a5a64b703eb61d170b6fef0e5af
Dan Mihai <dmihai@microsoft.com>
2026-07-05 19:21:10 +0000

    runtime implementation

--- src/runtime-rs/crates/runtimes/common/src/sandbox.rs
+++ src/runtime-rs/crates/runtimes/common/src/sandbox.rs
@@ -30,7 +30,13 @@ impl std::fmt::Debug for SandboxNetworkEnv {
 
 #[async_trait]
 pub trait Sandbox: Send + Sync {
-    async fn start(&self) -> Result<()>;
+    // async fn start(&self) -> Result<()>;
+    async fn start(
+        &self,
+        bundle_sandbox_id: Option<String>,
+        bundle_hostname: &str,
+    ) -> Result<()>;
+
     async fn start_template(&self) -> Result<()>;
     async fn stop(&self) -> Result<()>;
     async fn cleanup(&self) -> Result<()>;
