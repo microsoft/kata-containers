201dadee945e62c32b309a2c5705ece7a3a896f5
Dan Mihai <dmihai@microsoft.com>
2026-07-10 16:32:14 +0000

    enable sleep / timeout

--- src/runtime-rs/crates/resource/src/manager_inner.rs
+++ src/runtime-rs/crates/resource/src/manager_inner.rs
@@ -255,7 +255,10 @@ impl ResourceManagerInner {
         // tokio runtime, and block the task on it.
         let device_manager = self.device_manager.clone();
         let network = thread::spawn(move || -> Result<Arc<dyn Network>> {
-            let rt = runtime::Builder::new_current_thread().enable_io().build()?;
+            let rt = runtime::Builder::new_current_thread()
+                .enable_io()
+                .enable_time()
+                .build()?;
             let d = rt
                 .block_on(network::new(&network_config, device_manager))
                 .context("new network")?;
@@ -418,7 +421,10 @@ impl ResourceManagerInner {
         // then push interfaces/routes/neighbors to the agent.
         let device_manager = self.device_manager.clone();
         let network = thread::spawn(move || -> Result<Arc<dyn Network>> {
-            let rt = runtime::Builder::new_current_thread().enable_io().build()?;
+            let rt = runtime::Builder::new_current_thread()
+                .enable_io()
+                .enable_time()
+                .build()?;
             let d = rt
                 .block_on(network::new(&network_config, device_manager))
                 .context("new network")?;
